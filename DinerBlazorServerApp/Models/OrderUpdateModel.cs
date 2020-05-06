2694f80c738a0b9a0bbbee4e50e4b5789a682144
SoftwOrt Engineering <markus.sczeburek-ort@softwort-engineering.com>
2020-05-06 16:54:27 +0200

    Change Orderer Name is now possible
    Added EditForm to Display Page to change
    name of the Orderer in DB

---
+++ DinerBlazorServerApp/Models/OrderUpdateModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DinerBlazorServerApp.Models
+{
+    public class OrderUpdateModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20, ErrorMessage = "You need to keep the name to max of 20 Characters")]
+        [MinLength(3, ErrorMessage = "You need to enter at least 3 characters for an order name")]
+        [DisplayName("Name for the Order")]
+        public string OrderName { get; set; }
+    }
+}
